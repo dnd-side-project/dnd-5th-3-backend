b64dd35cb4ebcfbef0dc64d88954016cb2d9835b
KangSeongHyo <abc9023@naver.com>
2021-07-18 21:06:25 +0900

    [#003] feat: oauth2 인증 추가 (google)

---
+++ src/main/java/com/dnd5th3/dnd5th3backend/config/security/oauth2/CustomOAuth2UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.dnd5th3.dnd5th3backend.config.security.oauth2;
+
+import com.dnd5th3.dnd5th3backend.domain.member.Member;
+import com.dnd5th3.dnd5th3backend.domain.member.MemberType;
+import com.dnd5th3.dnd5th3backend.domain.member.Role;
+import com.dnd5th3.dnd5th3backend.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CustomOAuth2UserServiceImpl implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
+        String email = (String) oAuth2User.getAttributes().get("email");
+        String name = (String) oAuth2User.getAttributes().get("name");
+
+        if(email != null && name != null){
+            if(memberRepository.findByEmail(email) == null){
+                Member member = Member.builder()
+                        .email(email)
+                        .name(name)
+                        .role(Role.ROLE_USER)
+                        .memberType(MemberType.SOCIAL)
+                        .build();
+                memberRepository.save(member);
+            }
+        }
+
+        return oAuth2User;
+    }
+
+}
