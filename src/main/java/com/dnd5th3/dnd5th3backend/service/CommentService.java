6cac60fe6251c9d81fb3c64c3b3745d9f2e1ddee
KangSeongHyo <abc9023@naver.com>
2021-08-04 22:59:43 +0900

    [#24] feat: 댓글 수정 api 추가

--- src/main/java/com/dnd5th3/dnd5th3backend/service/CommentService.java
+++ src/main/java/com/dnd5th3/dnd5th3backend/service/CommentService.java
@@ -3,9 +3,28 @@ package com.dnd5th3.dnd5th3backend.service;
 import com.dnd5th3.dnd5th3backend.controller.dto.comment.CommentRequestDto;
 import com.dnd5th3.dnd5th3backend.domain.comment.Comment;
 import com.dnd5th3.dnd5th3backend.domain.member.Member;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.dnd5th3.dnd5th3backend.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface CommentService {
-    Comment saveComment(CommentRequestDto requestDto,Member member);
-    long updateComment(CommentRequestDto requestDto);
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public Comment saveComment(CommentRequestDto requestDto, Member member){
+        Comment comment = Comment.create(requestDto, member);
+        return commentRepository.save(comment);
+    }
+
+    @Transactional
+    public long updateComment(CommentRequestDto requestDto) {
+        Comment comment = commentRepository.findById(requestDto.getCommentId())
+                                           .orElseThrow(IllegalArgumentException::new);
+        comment.update(requestDto);
+        return comment.getId();
+    }
 }
---
+++ src/test/java/com/dnd5th3/dnd5th3backend/service/CommentServiceTest.java
@@ -0,0 +1,62 @@
+package com.dnd5th3.dnd5th3backend.service;
+
+import com.dnd5th3.dnd5th3backend.controller.dto.comment.CommentRequestDto;
+import com.dnd5th3.dnd5th3backend.domain.comment.Comment;
+import com.dnd5th3.dnd5th3backend.domain.member.Member;
+import com.dnd5th3.dnd5th3backend.domain.member.Role;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Import(CommentService.class)
+class CommentServiceTest {
+
+    @Autowired
+    private CommentService commentService;
+
+    private Member member;
+
+    @BeforeEach
+    public void setup(){
+        member = Member.builder()
+                .id(1L)
+                .email("test@gmail.com")
+                .password("1234")
+                .role(Role.ROLE_USER)
+                .name("닉네임")
+                .build();
+    }
+
+    @DisplayName("댓글 등록 테스트")
+    @Test
+    void saveComment() {
+        CommentRequestDto commentRequestDto = new CommentRequestDto(1L, 1L, 1L, 0, 0, "comment test");
+        Comment comment = commentService.saveComment(commentRequestDto, member);
+        
+        assertEquals(commentRequestDto.getContent(),comment.getContent(),"요청한 댓글 내용과 DB 저장된 댓글이 일치하는지 확인");
+    }
+
+    @DisplayName("댓글 수정 테스트")
+    @Test
+    //@Rollback(value = false)
+    void updateComment() {
+
+        CommentRequestDto commentRequestDto = new CommentRequestDto(1L, 1L, 1L, 0, 0, "comment test");
+        commentService.saveComment(commentRequestDto, member);
+
+        commentRequestDto = new CommentRequestDto(1L, 1L, 1L, 0, 0, "comment update");
+        long commentId = commentService.updateComment(commentRequestDto);
+
+        assertEquals(commentRequestDto.getCommentId(),commentId,"댓글 수정이 정상적으로 되었는지 확인");
+    }
+}
